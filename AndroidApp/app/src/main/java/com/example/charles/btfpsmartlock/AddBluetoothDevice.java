16441a49ab44acef42fd0b2094142a02045d4308
charlescqian <charles.miffy@gmail.com>
2018-07-28 00:39:30 -0700

    Recreated Projects and added Arduino (PlatformIO) Project

--- AndroidApp/app/src/main/java/com/example/charles/btfpsmartlock/AddBluetoothDevice.java
+++ AndroidApp/app/src/main/java/com/example/charles/btfpsmartlock/AddBluetoothDevice.java
@@ -1,7 +1,6 @@
 package com.example.charles.btfpsmartlock;
 
 import android.os.Bundle;
-import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,11 +10,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class AddBluetoothDevice extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +26,6 @@ public class AddBluetoothDevice extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
     @Override
@@ -59,10 +49,4 @@ public class AddBluetoothDevice extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# btfpsmartlock
-Bluetooth Smart Lock using Fingerprint Sensor with an Android phone. Prototype with Arduino Uno
